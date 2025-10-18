a9435dc2e750e3cb7f3050f11e99da7c14994d0f
unknown <22640121.quyen@student.iuh.edu.vn>
2025-10-18 16:50:51 +0700

    tao be

---
+++ src/main/java/com/fit/ecommercialmarketplacebe/ECommercialMarketPlaceBeApplication.java
@@ -0,0 +1,13 @@
+package com.fit.ecommercialmarketplacebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercialMarketPlaceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercialMarketPlaceBeApplication.class, args);
+    }
+
+}
