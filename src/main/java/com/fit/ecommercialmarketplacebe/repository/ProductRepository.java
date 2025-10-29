b36c2e4684468e5e56b7803ba8b09fb7e91ada39
unknown <22640121.quyen@student.iuh.edu.vn>
2025-10-29 16:15:05 +0700

    them CRUD product

--- src/main/java/com/fit/ecommercialmarketplacebe/repository/ProductRepository.java
+++ src/main/java/com/fit/ecommercialmarketplacebe/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.fit.ecommercialmarketplacebe.repository;
 
 import com.fit.ecommercialmarketplacebe.entity.Product;
+import com.fit.ecommercialmarketplacebe.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryNameIgnoreCase(String name);
-    // findProductByProductId(Long id) đã được JpaRepository cung cấp (dưới tên findById)
-    // Chúng ta sẽ dùng findById(id)
+    List<Product> findBySeller(Seller seller);
+
 }
